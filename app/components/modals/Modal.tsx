eccceaf5b96c4d085529efdd34df744c3b4faca4
Greg Sithole <gregsithole@hotmail.co.za>
2024-08-20 22:01:45 +0200

    Completed Modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -11,13 +11,13 @@ interface ModalProps {
 	title?: string;
 	body?: React.ReactElement;
 	footer?: React.ReactElement;
-	actionLabel?: string;
+	actionLabel: string;
 	disabled?: boolean;
 	secondaryAction?: () => void;
-	secondaryLabel?: string;
+	secondaryActionLabel?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryLabel }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryActionLabel }) => {
 
 	const [showModal, setShowModal] = useState(isOpen);
 
@@ -65,13 +65,13 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
 
 					<div className={`translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'}`}>
 						<div className="translate h-full md:h-auto lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-							<div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+							<div className="flex items-center p-4 rounded-t justify-center relative border-b-[1px]">
 
 								<button onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9">
 									<IoMdClose size={18} />
 								</button>
 
-								<div className="text-lg font-semibold">
+								<div className="text-lg font-bold">
 									{title}
 								</div>
 							</div>
@@ -82,7 +82,19 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
 
 							<div className="flex flex-col gap-2 p-6">
 								<div className="flex flex-row items-center gap-4 w-full">
-									<Button label="My Button" />
+									{secondaryAction && secondaryActionLabel && (
+										<Button
+											outline
+											disabled={disabled}
+											label={secondaryActionLabel}
+											onClick={handleSecondaryAction}
+										/>
+									)}
+									<Button
+										disabled={disabled}
+										label={actionLabel}
+										onClick={handleSubmit}
+									/>
 								</div>
 							</div>
 						</div>
