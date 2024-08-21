6e072bd370c2757e9aa0b21a8fd8fbc478bc27e4
Greg Sithole <gregsithole@hotmail.co.za>
2024-08-21 21:35:54 +0200

    Added inputs to Register Modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
+import toast from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal = () => {
 	const registerModal = useRegisterModal();
@@ -30,7 +32,7 @@ const RegisterModal = () => {
 		axios.post('/api/register', data).then((response) => {
 			registerModal.onClose();
 		}).catch((error) => {
-			console.log(error);
+			toast.error('Something went wrong. Please try again.');
 		}).finally(() => {
 			setIsLoading(false);
 		});
@@ -39,7 +41,27 @@ const RegisterModal = () => {
 	const bodyContent = (
 		<div className='flex flex-col gap-4'>
 			<Heading title="Welcome to Airbnb" />
-			<Input id="email" label="Email Address" disabled={isLoading} register={register} errors={errors} required />
+			<Input id="email" type='email' label="Email Address" disabled={isLoading} register={register} errors={errors} required />
+			<Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
+			<Input id="password" type='password' label="Password" disabled={isLoading} register={register} errors={errors} required />
+		</div>
+	)
+
+	const footerContent = (
+		<div className='flex flex-col gap-4 mt-3'>
+			<hr />
+			<Button outline label='Continue with Google' icon={FcGoogle} onClick={() => { }} />
+			<Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => { }} />
+			<div className='text-neutral-500 text-center mt-4 font-light'>
+				<div className='justify-center flex flex-row items-center gap-2'>
+					<div>
+						Already have an account?
+					</div>
+					<div onClick={registerModal.onClose} className='text-neutral-800 cursor-pointer hover:underline'>
+						Log in
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 
@@ -52,6 +74,7 @@ const RegisterModal = () => {
 			onClose={registerModal.onClose}
 			onSubmit={handleSubmit(onSubmit)}
 			body={bodyContent}
+			footer={footerContent}
 		/>
 	)
 }
