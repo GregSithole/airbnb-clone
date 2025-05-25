f8b0b8267c1594e8ef37d3a7e17756134f093b23
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-25 16:16:09 +0200

    Additional Updates

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,8 +6,14 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
-const UserMenu = () => {
+interface UserMenuProps {
+	currentUser?: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 	const registerModal = useRegisterModal();
 	const loginMoal = useLoginModal();
 	const [isOpen, setIsOpen] = useState(false);
@@ -32,10 +38,22 @@ const UserMenu = () => {
 			{isOpen && (
 				<div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
 					<div className="flex flex-col cursor-pointer">
-						<>
-							<MenuItem onClick={loginMoal.onOpen} label="Login" />
-							<MenuItem onClick={registerModal.onOpen} label="Sign up" />
-						</>
+						{currentUser ? (
+							<>
+								<MenuItem onClick={() => { }} label="My trips" />
+								<MenuItem onClick={() => { }} label="My favorites" />
+								<MenuItem onClick={() => { }} label="My reservations" />
+								<MenuItem onClick={() => { }} label="My properties" />
+								<MenuItem onClick={() => { }} label="Airbnb my home" />
+								<hr />
+								<MenuItem onClick={() => signOut()} label="Logout" />
+							</>
+						) : (
+							<>
+								<MenuItem onClick={loginMoal.onOpen} label="Login" />
+								<MenuItem onClick={registerModal.onOpen} label="Sign up" />
+							</>
+						)}
 					</div>
 				</div>
 			)}
