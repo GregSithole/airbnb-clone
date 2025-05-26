bd2add246e089ed3ac1f44aacddaf15cc949c11e
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-26 20:55:01 +0200

    Added Categories component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
+import Categories from './Categories';
 
 interface NavbarProps {
 	currentUser?: User | null;
@@ -22,6 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
 					</div>
 				</Container>
 			</div>
+			<Categories />
 		</div>
 	)
 }
