6d172f8dd6bd2574fd7142b4908b8e01b23ffa73
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-26 22:32:53 +0200

    Added rentModal to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 export const metadata: Metadata = {
 	title: "Airbnb Clone | Next.js & TailwindCSS",
@@ -32,6 +33,7 @@ export default async function RootLayout({ children }: Readonly<{
 					<ToasterProvider />
 					<RegisterModal />
 					<LoginModal />
+					<RentModal />
 					<Navbar currentUser={currentUser} />
 				</ClientOnly>
 				{children}
