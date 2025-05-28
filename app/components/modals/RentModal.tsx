33cdf0ec154232d73bafa8a3c39ac0431c635eb7
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-28 22:02:13 +0200

    Updated RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
+import dynamic from 'next/dynamic';
 
 enum STEPS {
 	CATEGORY = 0,
@@ -38,6 +39,11 @@ const RentModal = () => {
 	});
 
 	const category = watch('category');
+	const location = watch('location');
+
+	const Map = useMemo(() => dynamic(() => import('../Map'), {
+		ssr: false
+	}), [location]);
 
 	const setCustomValue = (id: string, value: any) => {
 		setValue(id, value, {
@@ -88,7 +94,8 @@ const RentModal = () => {
 		bodyContent = (
 			<div className='flex flex-col gap-8'>
 				<Heading title='Where is your place located?' subtitle='Help guests find you!' />
-				<CountrySelect />
+				<CountrySelect value={location} onChange={(value) => setCustomValue('location', value)} />
+				<Map center={location?.latlng} />
 			</div>
 		);
 	}
