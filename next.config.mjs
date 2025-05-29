c225f0c962aa7a61e54eaff0c7d5a32b34639fb3
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-29 17:22:30 +0200

    Added cloudinary to patterns

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
 			hostname: 'lh3.googleusercontent.com',
 			port: '',
 			pathname: '/**'
+		},
+		{
+			protocol: 'https',
+			hostname: 'res.cloudinary.com',
+			port: '',
+			pathname: '/**'
 		}]
 	}
 };
