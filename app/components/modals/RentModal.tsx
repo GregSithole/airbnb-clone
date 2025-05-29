3afdb1bf87280c62c27b371202a10073645c9b02
Greg Sithole <gregsithole@hotmail.co.za>
2025-05-29 11:41:07 +0200

    Added Counter Component

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,8 @@ import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
 	CATEGORY = 0,
@@ -40,6 +42,9 @@ const RentModal = () => {
 
 	const category = watch('category');
 	const location = watch('location');
+	const guestCount = watch('guestCount');
+	const roomCount = watch('roomCount');
+	const bathroomCount = watch('bathroomCount');
 
 	const Map = useMemo(() => dynamic(() => import('../Map'), {
 		ssr: false
@@ -96,6 +101,28 @@ const RentModal = () => {
 				<Heading title='Where is your place located?' subtitle='Help guests find you!' />
 				<CountrySelect value={location} onChange={(value) => setCustomValue('location', value)} />
 				<Map center={location?.latlng} />
+			</div>
+		);
+	}
+
+	if (step === STEPS.INFO) {
+		bodyContent = (
+			<div className='flex flex-col gap-8'>
+				<Heading title='Share some basic info about your place' subtitle='What amenities do you have?' />
+				<Counter title="Guests" subtitle='How many guest do you allow?' value={guestCount} onChange={(value) => setCustomValue('guestCount', value)} />
+				<hr />
+				<Counter title="Rooms" subtitle='How many rooms do you have?' value={roomCount} onChange={(value) => setCustomValue('roomCount', value)} />
+				<hr />
+				<Counter title="Bathrooms" subtitle='How many bathrooms do you have?' value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)} />
+			</div>
+		);
+	}
+
+	if (step === STEPS.IMAGE) {
+		bodyContent = (
+			<div className='flex flex-col gap-8'>
+				<Heading title='Add a photo of your place' subtitle='Show guests what your place looks like!' />
+				<ImageUpload />
 			</div>
 		);
 	}
